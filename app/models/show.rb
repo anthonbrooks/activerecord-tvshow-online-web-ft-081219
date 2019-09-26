3a273e8f077b1ba28e53427392c9dc68295b1385
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-26 19:01:21 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,23 +7,23 @@ class Show < ActiveRecord::Base
     self.where('rating = ?', self.highest_rating).first
   end 
   
-  def lowest_rating
+  def self.lowest_rating
     self.minimum(:rating)
   end 
   
-  def least_popular_show 
+  def self.least_popular_show 
     self.where('rating = ?', self.lowest_rating).first
   end 
   
-  def ratings_sum
+  def self.ratings_sum
     self.sum(:rating)
   end 
   
-  def popular_shows
+  def self.popular_shows
     self.where('rating > ?', 5)
   end 
   
-  def shows_by_alphabetical_order
+  def self.shows_by_alphabetical_order
     self.order(title: :desc)
   end 
 end 
